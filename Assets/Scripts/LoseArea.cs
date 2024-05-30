cd887f1762f5d55421d6faa0eb72e4e209b5b7f0
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-05-30 16:25:58 +0200

    🥝 Texture changes + warning blink

--- Assets/Scripts/LoseArea.cs
+++ Assets/Scripts/LoseArea.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 
 public class LoseArea : MonoBehaviour {
     /* -------------------------------- Variables ------------------------------- */
-    public float loseTime = 3f;
+    public float loseTime = 4f;
+    public float blinkTime = 0.3f;
     
+    [Header("Blinking settings")]
+    public Color blinkStartColor = Color.white;
+    public Color blinkEndColor = new Color(1f, 1f, 1f, 0.3f);
+    public float blinkSpeed = 8f;
+
+    // Private
     private Dictionary<GameObject, float> losingFruits;
     private GameManager gameManager;
 
-    
+
     /* ------------------------------- Unity Func ------------------------------- */
     // Start is called before the first frame update
     void Start() {
@@ -28,7 +35,12 @@ public class LoseArea : MonoBehaviour {
         float currentTime = losingFruits.GetValueOrDefault(collision.gameObject, 0f);
         losingFruits[collision.gameObject] = currentTime + Time.deltaTime;
 
-        // Check if fruit has been in losing area for too long
+        // Check if fruit has been in losing area for blinkTime
+        if (currentTime + Time.deltaTime >= blinkTime ) {
+            MakeFruitBlink(collision.gameObject);
+        }
+
+        // Check if fruit has been in losing area for loseTime
         if (currentTime + Time.deltaTime >= loseTime ) {
             // If fruit has been in for too long tell game manager that game has been lost
             if (gameManager.gameLost != true) {
@@ -40,6 +52,18 @@ public class LoseArea : MonoBehaviour {
 
     // Triggers when object exit the area
     void OnTriggerExit2D(Collider2D collision) {
-        losingFruits.Remove(collision.gameObject);
+        losingFruits.Remove(collision.gameObject); // Remove object from losingFruits dic
+        StopFruitBlink(collision.gameObject); // Revert fruit to normal color if was blinking
     }
+
+
+    /* -------------------------------- Functions ------------------------------- */
+    // Function that makes the fruit blink (Alpha)
+    void MakeFruitBlink(GameObject gameObject) {
+        gameObject.GetComponent<SpriteRenderer>().color = Color.Lerp(blinkStartColor, blinkEndColor, Mathf.PingPong(Time.time*blinkSpeed, 1));
+    }
+
+    void StopFruitBlink(GameObject gameObject) {
+        gameObject.GetComponent<SpriteRenderer>().color = Color.Lerp(gameObject.GetComponent<SpriteRenderer>().color, blinkStartColor, 1f);
+    }   
 }
